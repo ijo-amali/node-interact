65849f996f0eb7943c109b74184821e19e551f95
levistep32 <33301953+levistep32@users.noreply.github.com>
2018-11-24 19:22:29 -0700

    Update npm.js

--- npm.js
+++ npm.js
@@ -8,6 +8,8 @@ function search(arg) {
 	return execSync("npm search " + arg).toString()
 }
 
+// TODO: add functionality for a async version func
+// or use process.versions if possible
 function betterVersion() {
 	exec("npm --version", (err, out, stderr) => {
 		if (err || stderr) throw err
